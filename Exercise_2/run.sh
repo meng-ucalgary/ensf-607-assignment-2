2ac57c8f744c22ed5f403c63b78562d6b17b32f8
Bhavyai Gupta <zbhavyai@gmail.com>
2021-09-26 12:55:17 -0600

    Renaming Exercise 2 scripts

--- Exercise_2/run.sh
+++ Exercise_2/run.sh
@@ -7,7 +7,7 @@ clear
 set -o xtrace
 
 # compile the files
-javac -sourcepath Exercise_2 -d Exercise_2/bin Exercise_2/*java
+javac -sourcepath src -d bin src/*java
 
 # run
-java -cp Exercise_2/bin Drawing
+java -cp bin Drawing
